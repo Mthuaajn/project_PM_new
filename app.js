b44aa945f90f9a3ce87c50611b1950b0639e6165
21120612 <nmt6465@gmail.com>
2023-12-07 07:07:44 +0700

    create sever

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = express();
+
+process.noDeprecation = true;
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// middleware
+app.use(express.json());
+
+module.exports = app;

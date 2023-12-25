67ae7015d76cda288a05cf7f07e1b938abf4c2ae
21120612 <nmt6465@gmail.com>
2023-12-25 20:56:07 +0700

    ui sach

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const handlebars = require("express-handlebars");
 dotenv.config({ path: "./config.env" });
 const app = express();
 
+const qlsach = require("./routers/qlsach");
+const dlsach = require("./routers/dlsach");
+const nhapsach = require("./routers/nhapsach");
+const bansach = require("./routers/bansach");
+const timkiemsach = require("./routers/timkiemsach");
+
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+	app.use(morgan("dev"));
 }
 
 // template engine
 app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: ".hbs",
-  })
+	"hbs",
+	handlebars.engine({
+		extname: ".hbs",
+	})
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
@@ -25,8 +31,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// routers
+app.use("/qlsach", qlsach);
+app.use("/dlsach", dlsach);
+app.use("/nhapsach", nhapsach);
+app.use("/bansach", bansach);
+app.use("/timkiemsach", timkiemsach);
+
 app.get("/", (req, res) => {
-  res.render("home");
+	res.render("home");
 });
 
 module.exports = app;
